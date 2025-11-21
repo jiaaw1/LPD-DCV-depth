99a4e8a807ee08a5cdbe9c64a485d8bb7f058039
jiaa_w <147784954+jiaaw1@users.noreply.github.com>
2025-11-21 20:23:08 +0800

    Update import for ResnetEncoderDecoder

--- networks/__init__.py
+++ networks/__init__.py
@@ -1,4 +1,4 @@
 from .pose_decoder import PoseDecoder
 from .pose_encoder import PoseCNN
-from .resnet_encoder import ResnetEncoderDecoder
+from .resnet_encoder_lp_decoder import ResnetEncoderDecoder
 from .RDCV_transformer import RDCV_QueryTr
